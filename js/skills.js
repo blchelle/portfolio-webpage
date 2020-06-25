291abec4beee27d92a71fed55e3c1540a6d15941
Brock Chelle <brocklchelle@gmail.com>
2020-06-25 06:06:42 -0600

    skills section is mostly finished now, responsiveness and code clean up are left

--- js/skills.js
+++ js/skills.js
@@ -1,3 +1,14 @@
+function scrollContainerTo(pageNumber) {
+	const newPosition = pageNumber * skillsContainer.width() 
+	skillsContainer.animate(
+		{
+			scrollLeft: newPosition,
+		},
+		500,
+	);
+	currentPage = pageNumber;
+}
+
 function setCategoryClasses(element, array) {
 	element.addClass('selected');
 
@@ -10,19 +21,30 @@ function setCategoryClasses(element, array) {
 }
 
 function categorySelection(category) {
-	if (category === 'web') {
+	if (category === 'web' && currentPage !== 0) {
 		setCategoryClasses(webSelector, categorySelectors);
+		scrollContainerTo(0);
 	} else if (category === 'languages') {
 		setCategoryClasses(languagesSelector, categorySelectors);
+		scrollContainerTo(1);
 	} else if (category === 'databases') {
 		setCategoryClasses(databasesSelector, categorySelectors);
+		scrollContainerTo(2);
 	} else if (category === 'other') {
 		setCategoryClasses(otherSelector, categorySelectors);
+		scrollContainerTo(3);
 	}
 }
 
 // Get selectors for each of the category selectors
 const sectionSelectors = $('.skills__categories');
+const skillsContainer = $('.skills__container');
+
+// Get elements for all of the pages
+const webSkillsPage = $('.skills__page--web');
+const languagesSkillsPage = $('.skills__page--languages');
+const databasesSkillsPage = $('.skills__page--databases');
+const otherSkillsPage = $('.skills__page--other');
 
 const webSelector = $('.skills__category--web');
 const languagesSelector = $('.skills__category--languages');
@@ -33,3 +55,6 @@ webSelector.click(() => categorySelection('web'));
 languagesSelector.click(() => categorySelection('languages'));
 databasesSelector.click(() => categorySelection('databases'));
 otherSelector.click(() => categorySelection('other'));
+
+let currentPage = 0;
+$(window).resize(() => categorySelection('web'));
