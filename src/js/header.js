8e3d130a5ba8f0eb9c7e44d314cfc313211d1318
Brock Chelle <Brocklchelle@gmail.com>
2020-07-01 18:37:54 -0600

    started projects section

--- src/js/header.js
+++ src/js/header.js
@@ -1,66 +1,79 @@
 import $ from 'jquery';
 
-/**
- *
- * @param {*} el
- * @param {*} toRotate
- * @param {*} period
- */
-var typewriterAnimation = function (el, toRotate, period) {
-	this.toRotate = toRotate;
-	this.el = el;
-	this.loopNum = 0;
-	this.period = parseInt(period, 10) || 2000;
-	this.txt = '';
-	this.tick();
-	this.isDeleting = false;
-};
+class TypeWriterAnimation {
+	/**
+	 *
+	 * @param {JQuery} element The html element to apply the typewriter animation to
+	 * @param {String[]} textArray The array of strings to type
+	 * @param {Number} period The time period (in ms) to pause between typing and deleting
+	 */
+	constructor(element, textArray, period) {
+		this.element = element;
+		this.textArray = textArray;
+		this.period = period;
 
-typewriterAnimation.prototype.tick = function () {
-	var i = this.loopNum % this.toRotate.length;
-	var fullTxt = this.toRotate[i];
-
-	if (this.isDeleting) {
-		this.txt = fullTxt.substring(0, this.txt.length - 1);
-	} else {
-		this.txt = fullTxt.substring(0, this.txt.length + 1);
-	}
-
-	this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
-
-	var that = this;
-	var delta = 120 - Math.random() * 20;
-
-	if (this.isDeleting) {
-		delta /= 1.5;
-	}
-
-	if (!this.isDeleting && this.txt === fullTxt) {
-		delta = this.period;
-		this.isDeleting = true;
-	} else if (this.isDeleting && this.txt === '') {
+		this.loopNum = 0;
+		this.text = '';
 		this.isDeleting = false;
-		this.loopNum++;
-		delta = 500;
+		this.tick();
 	}
 
-	setTimeout(function () {
-		that.tick();
-	}, delta);
-};
+	/**
+	 * Animates the typing or deleting of a letter
+	 */
+	tick() {
+		// Get the index of the word being typed, and then get the word
+		const i = this.loopNum % this.textArray.length;
+		const fullText = this.textArray[i];
 
-window.onload = function () {
-	var elements = document.getElementsByClassName('typewriter-animation');
-	for (var i = 0; i < elements.length; i++) {
-		var toRotate = elements[i].getAttribute('data-rotate');
-		var period = elements[i].getAttribute('data-period');
-		if (toRotate) {
-			new typewriterAnimation(elements[i], JSON.parse(toRotate), period);
+		// If deleting, subtract a letter, otherwise add a letter
+		if (this.isDeleting) this.text = fullText.substring(0, this.text.length - 1);
+		else this.text = fullText.substring(0, this.text.length + 1);
+
+		// Set the html of the element to match the world
+		this.element.html(`<span class="wrap">${this.text}</span>`);
+
+		const next = this;
+
+		// Calculate the time in milliseconds to type the next letter (with a bit of randomness)
+		let typingSpeed = 60 + (Math.random() * 20 - 10);
+
+		// Make deleting twice as fast
+		if (this.isDeleting) typingSpeed /= 2;
+
+		// If the full word has been typed, set isDeleting to true
+		if (!this.isDeleting && this.text === fullText) {
+			typingSpeed = this.period;
+			this.isDeleting = true;
 		}
+		// If the full word has been deleted, set isDeleting to false
+		else if (this.isDeleting && this.text === '') {
+			this.isDeleting = false;
+			this.loopNum++;
+			typingSpeed = 500;
+		}
+
+		setTimeout(function () {
+			next.tick();
+		}, typingSpeed);
 	}
-	// INJECT CSS
-	var css = document.createElement('style');
+}
+
+$(window).ready(function () {
+	// Query for the roleText element to add the typewriter animation to
+	const roleText = $('#typewriter-animation');
+
+	// Pull the various texts and period off of the attribute
+	const toRotate = roleText.attr('data-rotate');
+	const period = roleText.attr('data-period');
+
+	if (toRotate) {
+		new TypeWriterAnimation(roleText, JSON.parse(toRotate), parseInt(period, 10) || 500);
+	}
+
+	// Inject css for the cursor
+	const css = document.createElement('style');
 	css.type = 'text/css';
-	css.innerHTML = '.typewriter-animation > .wrap { border-right: 0.08em solid var(--color-text)}';
+	css.innerHTML = '#typewriter-animation > .wrap { border-right: 0.1rem solid var(--color-text)}';
 	document.body.appendChild(css);
-};
+});
