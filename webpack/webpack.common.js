5e846578783184af2d7a3244f26ccc59813066a7
Brock Chelle <brocklchelle@gmail.com>
2020-09-09 08:00:43 -0600

    Updated favicon
    Added favicons for all devices

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -6,7 +6,7 @@ module.exports = {
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: path.resolve(__dirname, '../src/index.html'),
-			favicon: path.resolve(__dirname, '../src/favicon.ico'),
+			favicon: path.resolve(__dirname, '../src/assets/favicons/favicon.ico'),
 		}),
 	],
 	module: {
