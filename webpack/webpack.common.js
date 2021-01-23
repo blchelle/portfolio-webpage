7d4ca6766f17ef0cbe1ade7ca6103db91f164fc5
Brock Chelle <Brocklchelle@gmail.com>
2021-01-22 17:21:24 -0700

    Added scroll animations to many elements

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,5 +1,5 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
 	entry: path.resolve(__dirname, '../src/index.js'),
@@ -23,6 +23,10 @@ module.exports = {
 						},
 					},
 				],
+			},
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
 			},
 			{
 				test: /\.(woff|woff2|eot|ttf|otf)$/,
@@ -53,4 +57,4 @@ module.exports = {
 			},
 		],
 	},
-};
+}
