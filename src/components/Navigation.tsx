91b37abcdd8428977af3afb3e2bb2b67df4f01e7
Brock Chelle <brocklchelle@gmail.com>
2021-08-30 19:07:01 -0600

    #72: Fix mobile navbar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import NavigationItem, { INavigationItem } from '@components/NavigationItem';
 
 interface NavigationProps {
@@ -8,9 +8,23 @@ interface NavigationProps {
 }
 
 const Navigation: React.FC<NavigationProps> = ({ section, onSectionChange, sections }) => {
+	const [isMobile, setIsMobile] = useState(window.innerWidth < 700)
+
+	const handleResize = () => {
+		setIsMobile(window.innerWidth < 700)
+	}
+
+	useEffect(() => {
+		window.addEventListener('resize', handleResize);
+	}, [])
+
+	const sliderStyle = isMobile ?
+		{ left: `${(section * 100) / sections.length}%` } :
+		{ top: `${(section * 100) / sections.length}%` }
+
 	return (
-		<nav className='navbar'>
-			<div className='navbar__slider' style={{ top: `${(section * 100) / sections.length}%` }} />
+		<nav className={`navbar${isMobile ? '--mobile' : ''}`}>
+			<div className='navbar__slider' style={sliderStyle} />
 			{sections.map((item, index) => (
 				<NavigationItem
 					key={index}
