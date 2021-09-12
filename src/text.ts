defcead1ae688bf83dcab7b8df88075b2590ce80
Brock Chelle <brocklchelle@gmail.com>
2021-09-12 10:32:16 -0600

    #102: Refactor about me section to tailwind

--- src/text.ts
+++ src/text.ts
@@ -1,5 +1,16 @@
 export default {
-	about: {
+	aboutMe: {
+		part1: `I am a 4th year Software Engineering Student at the University of Alberta. I have experience
+						developing full-stack wep applications with React & Node as well as mobile apps for iOS
+						using Swift (iOS) and Java (Android).`,
+		part2: `Most recently I've been developing full-stack web applications using the popular MERN Stack.
+						I used these skills to build a team collaboration software similar to Trello or Jira.`,
+		part3: `Professionally, I have experience working as a software developer at Willowglen Systems, a
+						leader in the industrial automation industry. Here, I had the opportunity to work on life
+						saving products and gained valuable experience that will help me immensely throughout the
+						length of my career.`,
+	},
+	skills: {
 		description: `Since starting my software development journey, I've gained experience with a variety
 									technologies and I'm actively learning even more. During my most recent internship, I
 									learned how to develop front-end applications with Javascript and back-end applications with
