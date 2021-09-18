473c8bf9c7b04b8fc59bdecbae7252b501324a3f
Brock Chelle <brocklchelle@gmail.com>
2021-09-18 11:49:43 -0600

    #116: Add contact input component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,70 +1,14 @@
 import React, { ChangeEvent, useState } from 'react';
 import { ReactComponent as SendIcon } from '@assets/icons/send.svg';
-
-interface InputState {
-	error: boolean;
-	isEmpty: boolean;
-	value: string;
-}
+import TextInput from '@components/TextInput';
 
 const Form = () => {
-	const [name, setName] = useState<InputState>({ error: false, isEmpty: true, value: '' });
-	const [email, setEmail] = useState<InputState>({ error: false, isEmpty: true, value: '' });
-	const [message, setMessage] = useState<InputState>({ error: false, isEmpty: true, value: '' });
-
-	const handleInputChange =
-		(setter: React.Dispatch<React.SetStateAction<InputState>>) =>
-		(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-			const {
-				validity: { valid },
-				value,
-			} = event.target;
-			setter({ error: !valid, isEmpty: value.length === 0, value });
-		};
-
 	return (
 		<form method='POST' name='contact' className='contact__form' data-aos='fade-left'>
 			<input type='hidden' name='form-name' value='contact' />
-			<div className='contact__container contact__container--name'>
-				<label
-					className={`name__label ${name.error ? 'error' : ''} ${name.isEmpty ? '' : 'raised'}`}
-					htmlFor='name'
-				>
-					Name
-				</label>
-				<input type='text' name='name' id='name' required onChange={handleInputChange(setName)} />
-			</div>
-			<div className='contact__container contact__container--email'>
-				<label
-					htmlFor='email'
-					className={`${email.error ? 'error' : ''} ${email.isEmpty ? '' : 'raised'}`}
-				>
-					Email Address
-				</label>
-				<input
-					type='email'
-					name='email'
-					id='email'
-					required
-					onChange={handleInputChange(setEmail)}
-				/>
-			</div>
-			<div className='contact__container contact__container--message'>
-				<label
-					htmlFor='message'
-					className={`${message.error ? 'error' : ''} ${message.isEmpty ? '' : 'raised'}`}
-				>
-					Your Message
-				</label>
-				<textarea
-					name='message'
-					id='message'
-					cols={30}
-					rows={8}
-					required
-					onChange={handleInputChange(setMessage)}
-				/>
-			</div>
+			<TextInput label='Name' name='name' required />
+			<TextInput label='Email' name='email' required type='email' />
+			<TextInput label='Message' name='message' required numLines={6} />
 			<button type='submit'>
 				Send <SendIcon className='contact__send__icon' />
 			</button>
