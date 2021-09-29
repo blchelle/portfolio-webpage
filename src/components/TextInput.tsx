0997a7bc1d98dc605e5f058594294216a2c3e257
Brock Chelle <brocklchelle@gmail.com>
2021-09-29 07:10:12 -0600

    #133: Remove CSS Resets

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -39,12 +39,12 @@ const TextInput: React.FC<TextInputProps> = ({
 
 	const labelColor = error && visited ? 'text-error' : isEmpty ? 'text-gray-4' : 'text-primary';
 	const labelFontSize = isEmpty ? 'text-3xl' : 'text-2xl';
-	const labelTop = numLines === 1 ? '1.4rem' : '0.3rem';
+	const inputPaddingTop = numLines === 1 ? 'pt-0' : 'pt-4';
 	const inputBorderColor = error && visited ? 'border-error' : 'border-gray-5';
 	const inputHeight = numLines === 1 ? 'h-20' : 'h-auto';
 
-	const commonProps = {
-		className: `w-full ${inputHeight} px-4 bg-gray-1 ${inputBorderColor} border-2 border-solid rounded-md text-gray-4 text-3xl`,
+	const commonInputProps = {
+		className: `w-full ${inputHeight} ${inputPaddingTop} px-4 bg-gray-1 ${inputBorderColor} border-2 border-solid rounded-md text-gray-4 text-3xl`,
 		type: type,
 		name: name,
 		id: name,
@@ -57,15 +57,15 @@ const TextInput: React.FC<TextInputProps> = ({
 		<div className='relative w-full mb-12'>
 			<label
 				className={`absolute left-4 transition-all ${labelColor} ${labelFontSize}`}
-				style={{ top: isEmpty ? labelTop : '-2.2rem' }}
+				style={{ top: isEmpty ? '1.4rem' : '-2.2rem' }}
 				htmlFor={name}
 			>
 				{label}
 			</label>
 			{numLines === 1 ? (
-				<input {...commonProps} type={type} />
+				<input {...commonInputProps} type={type} />
 			) : (
-				<textarea {...commonProps} rows={numLines} />
+				<textarea {...commonInputProps} rows={numLines} />
 			)}
 		</div>
 	);
