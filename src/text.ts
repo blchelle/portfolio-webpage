f96d318e000cc55738c1336a72ff79173b912641
Brock Chelle <brocklchelle@gmail.com>
2021-09-29 19:07:54 -0600

    #80: Update text content

--- src/text.ts
+++ src/text.ts
@@ -1,23 +1,22 @@
 export default {
 	aboutMe: {
-		part1: `I am a 4th year Software Engineering Student at the University of Alberta. I have experience
-						developing full-stack wep applications with React & Node as well as mobile apps for iOS
+		part1: `I am a 5th year Software Engineering Student at the University of Alberta. I have experience
+						developing full-stack wep applications with React, Node, Rails, and Golang as well as mobile apps for iOS
 						using Swift (iOS) and Java (Android).`,
 		part2: `Most recently I've been developing full-stack web applications using the popular MERN Stack.
 						I used these skills to build a team collaboration software similar to Trello or Jira.`,
-		part3: `Professionally, I have experience working as a software developer at Willowglen Systems, a
-						leader in the industrial automation industry. Here, I had the opportunity to work on life
-						saving products and gained valuable experience that will help me immensely throughout the
-						length of my career.`,
+		part3: `Professionally, I have experience working as a software developer at Willowglen Systems and Tesla,
+						leaders in industrial automation and the electric vehicle industry. In these positions, I had the opportunity
+						to work on products that have saved lives and accelerated the transition to sustainable energy.`,
 	},
 	skills: {
 		description: `Since starting my software development journey, I've gained experience with a variety
-									technologies and I'm actively learning even more. During my most recent internship, I
-									learned how to develop front-end applications with Javascript and back-end applications with
-									C++.`,
+									technologies and I'm actively learning even more. During my current position with Tesla, I've
+									gained experience developing frontend applications with React and Javascript, backend
+									applications with Rails and Golang, and using Docker and Kubernetes for Devops.`,
 	},
 	contact: {
-		description: `I'm currently looking for 3-8 month internship opportunities starting based out of Edmonton,
-									Canada or Remotely. I look forward to hearing from you!`,
+		description: `I'm currently looking for full time positions as a full stack engineer starting in May 2022.
+									I look forward to hearing from you!`,
 	},
 };
