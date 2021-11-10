eb62630e01cdc7ea5bf4cfd29b6c6fdfea540c60
Brock Chelle <brocklchelle@gmail.com>
2021-11-10 15:21:52 -0700

    #159 Improved header animation

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -6,11 +6,11 @@ const Header = React.forwardRef<HTMLElement>((_, ref) => {
 	const circleOneRef = useRef<HTMLDivElement>(null);
 	const circleTwoRef = useRef<HTMLDivElement>(null);
 
-	const worstYellow = [240, 198, 45]
-	const bestGreen = [44, 160, 96]
-	const maxDistance = 500
+	const worstYellow = [240, 198, 45];
+	const bestGreen = [44, 160, 96];
+	const maxDistance = 500;
 
-	const [solved, setSolved] = useState(false)
+	const [solved, setSolved] = useState(false);
 
 	const scrollToStart = () => {
 		if (ref == null || typeof ref === 'function') return;
@@ -21,7 +21,7 @@ const Header = React.forwardRef<HTMLElement>((_, ref) => {
 	};
 
 	const moveTopInitial = (event: MouseEvent) => {
-		if (solved) return
+		if (solved) return;
 
 		const { current: circleOne } = circleOneRef;
 		const { current: circleTwo } = circleTwoRef;
@@ -29,44 +29,56 @@ const Header = React.forwardRef<HTMLElement>((_, ref) => {
 		if (!circleOne || !circleTwo) return;
 		if (ref === null || typeof ref === 'function' || !ref.current) return;
 
-		const centerX = ref.current.clientWidth / 2
-		const centerY = ref.current.clientHeight / 2
+		const centerX = ref.current.clientWidth / 2;
+		const centerY = ref.current.clientHeight / 2;
 
-		const distToCenterX = event.clientX - centerX
-		const distToCenterY = event.clientY - centerY
+		const distToCenterX = event.clientX - centerX;
+		const distToCenterY = event.clientY - centerY;
 
 		const distanceFromCenter = Math.sqrt(Math.pow(distToCenterX, 2) + Math.pow(distToCenterY, 2));
 
 		// Snaps the circles together if close enough
-		if (distanceFromCenter < 10) {
+		if (distanceFromCenter < 5) {
 			circleOne.style.left = `${centerX - circleOne.offsetWidth / 2}px`;
 			circleOne.style.bottom = `${centerY}px`;
 			circleTwo.style.right = `${centerX - circleOne.offsetWidth / 2}px`;
 			circleTwo.style.bottom = `${centerY}px`;
 
-			setSolved(true)
+			setSolved(true);
 		} else {
 			circleOne.style.left = `${event.clientX - circleOne.offsetWidth / 2}px`;
 			circleOne.style.bottom = `${event.clientY}px`;
 			circleTwo.style.right = `${event.clientX - circleTwo.offsetWidth / 2}px`;
 			circleTwo.style.bottom = `${ref.current.clientHeight - event.clientY}px`;
+			const red =
+				bestGreen[0] + (worstYellow[0] - bestGreen[0]) * (distanceFromCenter / maxDistance);
+			const green =
+				bestGreen[1] + (worstYellow[1] - bestGreen[1]) * (distanceFromCenter / maxDistance);
+			const blue =
+				bestGreen[2] - (bestGreen[2] - worstYellow[2]) * (distanceFromCenter / maxDistance);
+			ref.current.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 		}
-
-		const red = bestGreen[0] + (worstYellow[0] - bestGreen[0]) * (distanceFromCenter / maxDistance)
-		const green = bestGreen[1] + (worstYellow[1] - bestGreen[1]) * (distanceFromCenter / maxDistance)
-		const blue = bestGreen[2] - (bestGreen[2] - worstYellow[2]) * (distanceFromCenter / maxDistance)
-		ref.current.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`
 	};
 
 	return (
 		<header
 			ref={ref}
-			className='relative h-screen flex-center flex-col bg-header-far'
+			className='relative h-screen flex-center flex-col bg-header-far overflow-hidden'
 			onMouseMove={moveTopInitial}
 		>
-			<div className={`${solved ? 'bg-header-close' : 'shadow-2xl'} absolute header-circle left-1/2 bottom-1/4 transform translate-y-1/2`} ref={circleOneRef} />
-			<div className={`${solved ? 'bg-header-close' : 'shadow-2xl'} absolute header-circle right-1/2 bottom-3/4 transform translate-y-1/2`} ref={circleTwoRef} />
-			<div className='flex flex-col w-5/6 sm:w-3/4 z-10 p-8 bg-gray-transparent1 dark:bg-gray-transparent7 rounded-xl'>
+			<div
+				className={`${
+					solved ? 'solved' : ''
+				} absolute header-circle left-1/2 bottom-1/4 bg-gray-3 dark:bg-gray-6 opacity-75 shadow-2xl transform translate-y-1/2`}
+				ref={circleOneRef}
+			/>
+			<div
+				className={`${
+					solved ? 'solved' : ''
+				} absolute header-circle right-1/2 bottom-3/4 bg-gray-3 dark:bg-gray-6 opacity-75 shadow-2xl transform translate-y-1/2`}
+				ref={circleTwoRef}
+			/>
+			<div className='flex flex-col items-center w-5/6 sm:w-3/4 z-10 p-8 bg-gray-transparent3 dark:bg-gray-transparent6 shadow-xl rounded-xl'>
 				<h1 className='text-8xl font-bold px-12 mb-8'>
 					Hello, I'm
 					<span className='text-primary ml-4'>Brock Chelle</span>
