3aaa4a86c1ef4ae3411e42e3ea43572f912e0981
Brock Chelle <brocklchelle@gmail.com>
2021-11-10 09:33:04 -0700

    #157: Fix light/dark theme toggler

--- src/components/ThemeToggler.tsx
+++ src/components/ThemeToggler.tsx
@@ -29,15 +29,15 @@ const ThemeToggler = () => {
 		}
 	};
 
-	const leftStyle = theme === 'light' ? { left: '0.4rem' } : { left: '4.3rem' };
+	const leftStyle = theme === 'light' ? { left: '0.25rem' } : { left: '4.1rem' };
 
 	return (
 		<div
-			className='fixed z-10 top-8 right-8 w-36 h-20 rounded-full bg-gray-3 dark:bg-gray-7 shadow-xl border border-blue-500 dark:border-yellow-400 '
+			className='fixed top-8 right-8 w-32 h-16 rounded-full bg-gray-3 dark:bg-gray-7 shadow-xl border border-blue-500 dark:border-yellow-400 z-20'
 			onClick={toggleTheme}
 		>
 			<div
-				className={`border-blue-500 bg-blue-200 dark:border-yellow-400 dark:bg-yellow-200 border-4 absolute w-16 h-16 top-1/2 transform -translate-y-1/2 rounded-full transition-all z-30`}
+				className={`border-blue-500 bg-blue-200 dark:border-yellow-400 dark:bg-yellow-200 border-4 absolute w-14 h-14 top-1/2 transform -translate-y-1/2 rounded-full transition-all z-30`}
 				style={leftStyle}
 			/>
 			<SunIcon className='absolute left-3 w-12 h-12 top-1/2 transform -translate-y-1/2 z-20 stroke-current text-yellow-400' />
