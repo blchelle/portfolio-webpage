d2ebcdf569c5e949cee89a71778d51de564017d9
Brock Chelle <brocklchelle@gmail.com>
2021-11-18 07:55:52 -0700

    #193: Add tailwindcss-classnames dependency

--- src/components/Tool.tsx
+++ src/components/Tool.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Tooltip from 'react-tooltip';
+import { classnames } from 'tailwindcss-classnames';
 
 export interface ITool {
 	name: string;
@@ -11,10 +12,13 @@ interface ToolProps {
 }
 
 const Tool: React.FC<ToolProps> = ({ tool: { name, Logo } }) => {
+	const containerClasses = classnames('inline-block', 'mr-5');
+	const logoClasses = classnames('h-12', 'w-auto', 'text-primary', 'fill-current');
+
 	return (
-		<div className='inline-block mr-5'>
+		<div className={containerClasses}>
 			<Tooltip effect='solid' />
-			<Logo className='h-12 w-auto text-primary fill-current' data-tip={name} />
+			<Logo className={logoClasses} data-tip={name} />
 		</div>
 	);
 };
