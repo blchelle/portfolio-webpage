22978ead1a171e01e2a4bcff06cd02d3127ae28f
Brock Chelle <brocklchelle@gmail.com>
2021-11-30 21:21:15 -0700

    #203: Refactor tailwind styles for <Highlight />, <Form />

---
+++ src/tailwind/Highlight.ts
@@ -0,0 +1,11 @@
+import { classnames } from 'tailwindcss-classnames';
+
+export default {
+	text: classnames(
+		'text-gray-5',
+		'dark:text-gray-1',
+		'hover:text-primary',
+		'font-bold',
+		'transition-colors'
+	),
+};
