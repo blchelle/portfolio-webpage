2aa2ce82db0d1d0eddd0ae84a8a3e524b19e4323
Brock Chelle <brocklchelle@gmail.com>
2023-03-08 13:36:29 -0700

    Add elastic, prometheus to skills

--- src/content/skills.ts
+++ src/content/skills.ts
@@ -7,6 +7,7 @@ import { ReactComponent as concourseLogo } from '@assets/logos/concourse.svg';
 import { ReactComponent as cssLogo } from '@assets/logos/css.svg';
 import { ReactComponent as dockerLogo } from '@assets/logos/docker.svg';
 import { ReactComponent as expressLogo } from '@assets/logos/express.svg';
+import { ReactComponent as elasticLogo } from '@assets/logos/elastic.svg';
 import { ReactComponent as firebaseLogo } from '@assets/logos/firebase.svg';
 import { ReactComponent as goLogo } from '@assets/logos/go.svg';
 import { ReactComponent as gitLogo } from '@assets/logos/git.svg';
@@ -14,12 +15,12 @@ import { ReactComponent as grafanaLogo } from '@assets/logos/grafana.svg';
 import { ReactComponent as htmlLogo } from '@assets/logos/html.svg';
 import { ReactComponent as jsLogo } from '@assets/logos/javascript.svg';
 import { ReactComponent as kubernetesLogo } from '@assets/logos/kubernetes.svg';
-import { ReactComponent as mariaDbLogo } from '@assets/logos/mariadb.svg';
 import { ReactComponent as mongoDbLogo } from '@assets/logos/mongodb.svg';
 import { ReactComponent as netlifyLogo } from '@assets/logos/netlify.svg';
 import { ReactComponent as mysqlLogo } from '@assets/logos/mysql.svg';
 import { ReactComponent as nodeLogo } from '@assets/logos/node.svg';
 import { ReactComponent as postgresqlLogo } from '@assets/logos/postgresql.svg';
+import { ReactComponent as prometheusLogo } from '@assets/logos/prometheus.svg';
 import { ReactComponent as protobufLogo } from '@assets/logos/protobuf.svg';
 import { ReactComponent as reactLogo } from '@assets/logos/react.svg';
 import { ReactComponent as railsLogo } from '@assets/logos/rails.svg';
@@ -29,7 +30,6 @@ import { ReactComponent as rubyLogo } from '@assets/logos/ruby.svg';
 import { ReactComponent as sassLogo } from '@assets/logos/sass.svg';
 import { ReactComponent as sidekiqLogo } from '@assets/logos/sidekiq.svg';
 import { ReactComponent as splunkLogo } from '@assets/logos/splunk.svg';
-import { ReactComponent as sqliteLogo } from '@assets/logos/sqlite.svg';
 import { ReactComponent as tailwindLogo } from '@assets/logos/tailwind.svg';
 import { ReactComponent as tsLogo } from '@assets/logos/typescript.svg';
 import { ReactComponent as webpackLogo } from '@assets/logos/webpack.svg';
@@ -74,8 +74,8 @@ const SKILLS: ISkillCategory[] = [
 			{ name: 'Redis', Logo: redisLogo },
 			{ name: 'MySQL', Logo: mysqlLogo },
 			{ name: 'PostgreSQL', Logo: postgresqlLogo },
-			{ name: 'MariaDB', Logo: mariaDbLogo },
-			{ name: 'SQLite', Logo: sqliteLogo },
+			{ name: 'Elastic', Logo: elasticLogo },
+			{ name: 'Prometheus', Logo: prometheusLogo },
 			{ name: 'Firebase', Logo: firebaseLogo },
 		],
 	},
